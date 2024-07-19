9855cdf04d115be914502309b58c1f3d085c1bd1
sonicspeed1 <150836455+sonicspeed1@users.noreply.github.com>
2024-07-19 12:54:49 -0500

    Update pedidoroute.js

--- backend/src/routes/pedidoroute.js
+++ backend/src/routes/pedidoroute.js
@@ -23,4 +23,26 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/obtener', async (req, res) => {
+    try {
+        const pedidos = await Pedido.find();
+        res.status(200).json(pedidos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const idPedido = req.params.id;
+
+    try {
+        const pedidoEliminado = await Pedido.findByIdAndDelete(idPedido);
+        if (!pedidoEliminado) {
+            return res.status(404).json({ message: 'Pedido no encontrado' });
+        }
+        res.status(200).json({ message: 'Pedido eliminado correctamente' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 module.exports = router;
